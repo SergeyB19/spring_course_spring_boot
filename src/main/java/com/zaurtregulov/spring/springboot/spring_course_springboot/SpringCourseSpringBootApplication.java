ec4078c63f99d67169fe90158e86d3a4a3ad1261
my.h.m@yandex.ru <Zxcv7788jetbrains>
2023-06-30 07:49:40 +0300

    Initial commit

---
+++ src/main/java/com/zaurtregulov/spring/springboot/spring_course_springboot/SpringCourseSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.zaurtregulov.spring.springboot.spring_course_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCourseSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCourseSpringBootApplication.class, args);
+	}
+
+}
